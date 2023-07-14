4da31c20e9ab30ed97cd8c1a4402ce90fadc164f
John Whittington <git@jbrengineering.co.uk>
2023-07-14 15:46:47 +0200

    support arm64 darwin homebrew and brow x86_64

--- support/prompts/max.zsh
+++ support/prompts/max.zsh
@@ -32,8 +32,8 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    os_icon               # os identifier
-    # example
+    # os_icon               # os identifier
+    example
     dir                     # current directory
     vcs                     # git status
     context
